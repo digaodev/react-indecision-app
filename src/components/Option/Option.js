ad2f9ff314a4cea22c1242a761e4af43e1281892
digaodev <rodrigo.rb.dev@gmail.com>
2018-05-18 16:35:35 -0300

    Refactor: add Option component styles using styled components.

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,12 +1,41 @@
 import React from 'react';
 
+// 3rd PARTY PACKAGES
+import styled from 'styled-components';
+
+const ListItem = styled.li`
+  background-color: #1c313a;
+  border-bottom: 1px solid #444;
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+`;
+
+const Button = styled.button`
+  border: none;
+  background-color: inherit;
+  color: #69e2ff;
+  cursor: pointer;
+  outline: none;
+  transition: color 0.2s ease-in-out;
+
+  &:hover,
+  :focus {
+    color: #ddd;
+  }
+
+  &:active {
+    color: #fff;
+  }
+`;
+
 const Option = ({ option, handleDeleteOption }) => (
-  <li className="options-list__item">
+  <ListItem className="options-list__item">
     {option.text}
-    <button type="button" onClick={() => handleDeleteOption(option.id)}>
-      X
-    </button>
-  </li>
+    <Button type="button" onClick={() => handleDeleteOption(option.id)}>
+      Remove
+    </Button>
+  </ListItem>
 );
 
 export default Option;
