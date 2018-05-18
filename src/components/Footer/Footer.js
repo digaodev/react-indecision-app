4d2214ddffffe949806d8e92a0bf00ac788d5f90
digaodev <rodrigo.rb.dev@gmail.com>
2018-05-18 16:57:23 -0300

    Refactor: add Footer component styles using styled components.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,39 @@
 import React from 'react';
 
+// 3rd PARTY PACKAGES
+import styled from 'styled-components';
+
+const Container = styled.footer`
+  background-color: #1c313a;
+  padding: 1rem;
+  font-size: 0.8rem;
+  text-align: center;
+`;
+
+const Paragraph = styled.h1`
+  letter-spacing: 0.2rem;
+`;
+
+const Link = styled.a`
+  color: #69e2ff;
+  text-decoration: none;
+`;
+
+
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>
+    <Container>
+      <Paragraph>
         Made by{' '}
-        <a
+        <Link
           href="https://github.com/digaodev/"
           target="_blank"
           rel="noopener noreferrer"
         >
           @digaodev
-        </a>
-      </p>
-    </footer>
+        </Link>
+      </Paragraph>
+    </Container>
   );
 };
 
