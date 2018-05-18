c742f11a588343ffc2127c5d557b9f575536444c
digaodev <rodrigo.rb.dev@gmail.com>
2018-05-18 15:16:44 -0300

    Refactor: add Header component styles using styled components.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
+// 3rd PARTY PACKAGES
+import styled from 'styled-components';
+
+const Container = styled.section`
+  background-color: #1c313a;
+  padding: 2rem;
+`;
+
+const Title = styled.h1`
+  letter-spacing: 0.2rem;
+  margin-bottom: 0.5rem;
+`;
+
+const SubTitle = styled.h2`
+  color: #69e2ff;
+  font-size: 1.8rem;
+  font-weight: normal;
+`;
+
 const Header = () => {
   return (
-    <header className="header">
-      <h1 className="header__title">Indecision App</h1>
-      <h2>Take the indecision out of your daily task management</h2>
-      <p className="header__subtitle">
-        So you've read 'Essentialism' and decided you are focusing on too many
-        things to be efficient. Don't worry, delegate your tasks to a random
-        picker and start focusing!
-      </p>
-    </header>
+    <Container>
+      <Title>Indecision</Title>
+      <SubTitle>Take the indecision out of your daily task management</SubTitle>
+    </Container>
   );
 };
-
 
 export default Header;
