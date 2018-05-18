5b70d4d07f38d4e7df03083a9eadb88767988e41
digaodev <rodrigo.rb.dev@gmail.com>
2018-05-18 16:35:24 -0300

    Refactor: add OptionList component styles using styled components.

--- src/components/OptionsList/OptionsList.js
+++ src/components/OptionsList/OptionsList.js
@@ -1,17 +1,59 @@
 import React from 'react';
 
+// 3rd PARTY PACKAGES
+import styled from 'styled-components';
+
 import Option from '../Option/Option';
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+const HeaderContainer = styled.header`
+  background-color: #102027;
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+`;
+
+const Title = styled.h3`
+  color: #69e2ff;
+  font-size: 1.8rem;
+  flex: 1;
+`;
+
+const Button = styled.button`
+  border: none;
+  background-color: inherit;
+  color: #69e2ff;
+  cursor: pointer;
+  outline: none;
+  transition: color 0.2s ease-in-out;
+
+  &:hover,
+  :focus {
+    color: #ddd;
+  }
+
+  &:active {
+    color: #fff;
+  }
+`;
 
 const OptionsList = ({
   options,
   handleDeleteOption,
   handleDeleteAllOptions
 }) => (
-  <section className="options-list">
-  <h3>Your options</h3>
-    {options.length > 0 && (
-      <button onClick={handleDeleteAllOptions}>Remove All Options</button>
-    )}
+  <Container className="options-list">
+    <HeaderContainer>
+      <Title>Your options</Title>
+      {options.length > 0 && (
+        <Button onClick={handleDeleteAllOptions}>Remove All Options</Button>
+      )}
+    </HeaderContainer>
+
     <ul>
       {options.map(option => {
         return (
@@ -23,7 +65,7 @@ const OptionsList = ({
         );
       })}
     </ul>
-  </section>
+  </Container>
 );
 
 export default OptionsList;
